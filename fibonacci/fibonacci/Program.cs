f48423c04def40e17a51ca6a6670b1471d9322fb
danielmarques25 <danielmarques2502@gmail.com>
2016-06-10 11:52:17 -0300

    Fibonacci Recursiva

---
+++ fibonacci/fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fibonacci(10));
+            Console.ReadKey();
+        }
+
+        static int fibonacci(int x)
+        {
+            if (x == 1 || x == 2)
+            {
+                return 1;
+            }
+
+            return fibonacci(x - 1) + fibonacci(x - 2);
+        }
+    }
+}
